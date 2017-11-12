2e9b07f7fd2350056976caf2f44dcf90f456030a
Haripriyan <haripriyan@live.com>
2017-11-12 20:41:01 +0530

    Hello Haskell

---
+++ Intro/HelloHaskell.hs
@@ -0,0 +1,5 @@
+-- In Haskell source file, you dont use a'let' to define top level variable
+string1 = "hello"
+string2 = "world"
+-- ++ is the string concat operator
+greeting = string1 ++ " " ++ string2 
