e089d999c6a7d8642eb48503d889ada880e7188b
Haripriyan <haripriyan@live.com>
2017-11-13 22:13:17 +0530

    Creating functions in Haskell

---
+++ Intro/CreatingFunctions.hs
@@ -0,0 +1,20 @@
+-- Creating functions in Haskell
+-- <function_name> <param1> <param2> ... <paramN> = <function_body>
+
+-- Function for finding Maximum of 3 numbers
+maxOf3 x y z = max (max x y) z
+
+-- Using conditionals. The Indents are important
+oddOrEven x =
+  if (mod x 2) == 0
+  then "Even"
+  else "Odd"
+
+-- Using recursions
+-- Haskell doesnot have loops unlike imperative languages
+-- Note that the parentheses are important for letting the compiler know the order of precedence
+-- By default the function calls are given the highest priority 
+powerOf2 x =
+  if x == 0
+  then 1
+  else 2 * (powerOf2 (x-1))
--- Intro/HelloHaskell.hs
+++ Intro/HelloHaskell.hs
@@ -1,5 +1,7 @@
 -- In Haskell source file, you dont use a'let' to define top level variable
+-- In GHCi, need to use 'let' to distinguish that you are declaring a new variable and not calling an existing one
 string1 = "hello"
 string2 = "world"
+
 -- ++ is the string concat operator
 greeting = string1 ++ " " ++ string2 
