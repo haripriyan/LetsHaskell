81a1acb84c3e5165528e0b5c19e85629af049000
Haripriyan <haripriyan@live.com>
2017-12-02 17:58:28 +0530

    Indentation is important'

--- Intro/2_CreatingFunctions.hs
+++ Intro/2_CreatingFunctions.hs
@@ -18,3 +18,21 @@ powerOf2 x =
   if x == 0
   then 1
   else 2 * (powerOf2 (x-1))
+
+-- Watch the Spaces!!
+-- There is also a small matter of the spaces having more meaning in Haskell than many others
+-- For saneness, don't use tabs. EVER
+-- Haskell compiler and your editor might interpret the tabs differently.
+-- Hence a function that looks ok in your editor might fail compiling.
+
+-- The indentation is important!!
+-- If you try the below function, it will not compile
+--- ======================================== ---
+--powerOf2 x =
+--    if x == 0
+--  then 1
+--    else 2 * (powerOf2 (x-1))
+--- ======================================== ---
+
+-- This is because the 'then' is not indented properly
+-- When breaking up any expressions or cases, the second line should be atleast the same indentation as the previous one, if not more.
